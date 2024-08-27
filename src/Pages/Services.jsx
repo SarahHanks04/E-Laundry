187ff37cd3b362bae9fa23ff6f701a0107fea654
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-28 00:39:45 +0100

    updated

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import Service3 from "../assets/Service3.png";
 import "../Styles/HomepageStyle.css";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Service = () => {
   const navigate = useNavigate();
   return (
-    <div className="homepage-container flex flex-col md:flex-row items-center p-8 bg-white">
-      <div className="image-container md:w-1/2 flex justify-center mt-8 md:mt-0">
+    <div className="homepage-container flex flex-col md:flex-row items-center p-4 md:p-8 bg-white">
+      <div className="image-container w-full md:w-1/2 flex justify-center mt-4 md:mt-0">
         <img
           src={Service3}
           alt="Friendly Laundromat"
-          width={450}
-          height={200}
-          className="max-w-full rounded-lg shadow-lg"
+          width={300}
+          height={150}
+          className="w-full max-w-xs md:max-w-md rounded-lg shadow-lg"
         />
       </div>
 
-      <div className="md:w-1/2">
-        <h1 className="text-4xl font-bold text-blue-950">
+      <div className="w-full md:w-1/2 mt-6 md:mt-0 text-center md:text-left">
+        <h1 className="text-3xl md:text-4xl font-bold text-blue-950">
           Get the <br /> <span> VIP Treatment</span>
         </h1>
-        <p className="mt-4 text-lg text-gray-600">
+        <p className="mt-4 text-sm md:text-md text-gray-600">
           Our team consists of skilled professionals who are passionate about
           what they do. They handle your garments with the utmost care and
-          attention, ensuring thorough cleaning, ironing and folding. With their
-          expertise, you can trust that your clothes will be handled
-          professionally and returned returned to you in pristine condition.
+          attention, ensuring thorough cleaning, ironing, and folding. With
+          their expertise, you can trust that your clothes will be handled
+          professionally and returned to you in pristine condition.
         </p>
-        <div className="mt-8 flex space-x-4">
+        <div className="flex justify-center md:justify-start space-x-4">
           <button
             onClick={() => navigate("/become-vip")}
-            className="mt-8 bg-blue-950 text-white px-6 py-2 rounded-full"
+            className="mt-6 md:mt-8 bg-blue-950 text-white px-4 py-2 md:px-6 md:py-2 rounded-full"
           >
             Become VIP
           </button>

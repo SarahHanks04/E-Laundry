05fd0aa1995d5e0aeab326c6ba8eff88a91e4e79
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-27 01:42:53 +0100

    updated

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "../Styles/About.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandshake, faTruck, faTruckFast, faUserTie } from '@fortawesome/free-solid-svg-icons';
 
 const AboutPage = () => {
   return (
@@ -14,30 +16,31 @@ const AboutPage = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="bg-white rounded-lg shadow-lg p-6">
           <div className="flex justify-center mb-4">
-            <span>&#129309;</span>
+            <span><FontAwesomeIcon icon={faHandshake} size="3x" /></span>
           </div>
-          <h2 className="text-2xl font-bold text-navy text-center mb-4">Our Commitment</h2>
-          <p className="text-gray-700 text-center">
+          <h2 className="text-2xl font-bold mb-4">Our Commitment</h2>
+          <p className="text-gray-700">
             At That Laundry Shop, we not only understand the significance of clean and fresh laundry but also prioritize personal hygiene. We believe that every customer deserves the highest level of care and attention.
           </p>
         </div>
         
         <div className="bg-white rounded-lg shadow-lg p-6">
           <div className="flex justify-center mb-4">
-            <img src="/path/to/pickup-delivery-icon.png" alt="Pickup and Delivery Service" className="h-12 w-12"/>
+            <span><FontAwesomeIcon icon={faTruckFast} size="3x" /></span>
           </div>
-          <h2 className="text-2xl font-bold text-navy text-center mb-4">Pickup and Delivery Service</h2>
-          <p className="text-gray-700 text-center">
+          <div className="px-4 h-[1px] w-[30px] text-black"></div>
+          <h2 className="text-2xl font-bold mb-4">Pickup and Delivery Service</h2>
+          <p className="text-gray-700">
             We offer reliable pickup and delivery services. Simply schedule a pickup at a time that works best for you, and our riders will ensure seamless and timely service.
           </p>
         </div>
         
         <div className="bg-white rounded-lg shadow-lg p-6">
           <div className="flex justify-center mb-4">
-            <img src="/path/to/expert-staff-icon.png" alt="Expert Staff" className="h-12 w-12"/>
+            <span><FontAwesomeIcon icon={faUserTie} size="3x" /></span>
           </div>
-          <h2 className="text-2xl font-bold text-navy text-center mb-4">Expert Staff</h2>
-          <p className="text-gray-700 text-center">
+          <h2 className="text-2xl font-bold mb-4">Expert Staff</h2>
+          <p className="text-gray-700">
             Our team consists of skilled professionals who handle your garments with the utmost care and attention, ensuring thorough cleaning, ironing, and folding.
           </p>
         </div>

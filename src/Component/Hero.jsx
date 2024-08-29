2d6211b8c80dadb99d42bcf26524b6821bac1893
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-30 00:55:54 +0100

    updated

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import Navbar from './Header'
 import HomePage from '../Pages/HomePage'
 
 const Hero = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <HomePage />
     </div>
   )

c1d2ee8b4df9c5ceb666164c5ac523ff19869d7a
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-31 01:46:58 +0100

    updated

---
+++ src/Component/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <div className="text-white bg-blue-950 py-2 hidden md:block">
+      <p className="text-center">+66 94 691 6668</p>
+    </div>
+  );
+};
+
+export default TopBar;
